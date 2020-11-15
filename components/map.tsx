c080a4f686f520418889de6697da307da64d2941
Michael Eliot <michaeleliot@Michaels-MacBook-Pro.local>
2020-11-14 16:57:53 -0800

    Added coloring to map via mapbox vector hosting.

--- components/map.tsx
+++ components/map.tsx
@@ -3,33 +3,9 @@ import React, { useRef, useEffect } from "react";
 import mapboxgl from "mapbox-gl";
 import Popup from "./popup";
 import styles from '../styles/map.module.css'
-
 //random-points-data
 
-const formatData = locations => {
-    const newFeaturesList = [];
-    for (let location of locations) {
-        const { id, lat, lng, name } = location;
-        newFeaturesList.push({
-            type: "Feature",
-            geometry: {
-                type: "Point",
-                coordinates: [lng, lat]
-            },
-            properties: {
-                id,
-                name: name,
-                description: `description for Waffle House #${id}`
-            }
-        });
-    }
-    return {
-        type: "FeatureCollection",
-        features: newFeaturesList
-    };
-};
-
-const Map = ({ authToken, data }) => {
+const Map = ({ authToken }) => {
     mapboxgl.accessToken = authToken;
     const mapContainerRef = useRef(null);
     const popUpRef = useRef(new mapboxgl.Popup({ offset: 15 }));
@@ -40,35 +16,44 @@ const Map = ({ authToken, data }) => {
             container: mapContainerRef.current,
             // See style options here: https://docs.mapbox.com/api/maps/#styles
             style: "mapbox://styles/mapbox/streets-v11",
-            center: [-90.5795, 39.8283],
-            zoom: 3
+            center: [-85.5795, 35.8283],
+            zoom: 4
         });
 
         map.addControl(new mapboxgl.NavigationControl(), "bottom-right");
 
         map.on("load", () => {
-            map.addSource("wafflehouse-data", {
-                type: "geojson",
-                data: {
-                    type: "FeatureCollection",
-                    features: []
-                }
-            });
-            map.getSource("wafflehouse-data").setData(formatData(data));
-            map.addLayer({
-                id: "wafflehouse-layer",
-                source: "wafflehouse-data",
-                type: "symbol",
-                layout: {
-                    // full list of icons here: https://labs.mapbox.com/maki-icons
-                    "icon-image": "bakery-15",
-                    "icon-padding": 0,
-                    "icon-allow-overlap": true,
-                },
-                paint: {
-                    "icon-color": " #ffffff"
-                }
-            });
+            map.loadImage('marker.png', function (error, image) {
+                if (error) throw error;
+                map.addImage('marker', image, { 'sdf': true });
+                map.addSource("wafflehouse-data", {
+                    'type': 'vector',
+                    'url': 'mapbox://michaeleliot.6xhri9rg'
+                });
+                map.addLayer({
+                    id: "wafflehouse-layer",
+                    source: "wafflehouse-data",
+                    'source-layer': "wafflehouse-4onydw",
+                    type: "symbol",
+                    layout: {
+                        // full list of icons here: https://labs.mapbox.com/maki-icons
+                        "icon-image": "marker",
+                        "icon-padding": 0,
+                        "icon-allow-overlap": true,
+                    },
+                    paint: {
+                        "icon-color": [
+                            "match",
+                            ['get', 'status'],
+                            'Good', "#7CFC00",
+                            'Medium', "#ff5733",
+                            "#f6ff33"
+                        ]
+                    }
+                });
+            })
+
+
         });
 
         map.on("mouseenter", "wafflehouse-layer", e => {
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,56 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 import Map from '../components/map';
-export default function Home(props) {
+import fs from 'fs'
+
+export default function Home({ token }) {
   return (
     <div className={styles.container}>
-      <Map data={props.waffleHouses} authToken={props.token} />
+      <Map authToken={token} />
+      <div>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
     </div>
   );
 }
 
 export async function getStaticProps(context) {
-  let results = await fetch(
-    'https://wafflehouse.locally.com/stores/conversion_data?has_data=true&company_id=117995&map_distance_diag=99999999',
-  ).then((data) => data.json());
-  let waffleHouses = results.markers;
-  console.log(waffleHouses);
+  const formatData = locations => {
+    const newFeaturesList = [];
+    for (let location of locations) {
+      const { id, lat, lng, name, is_temporarily_closed } = location;
+      let status = "Bad"
+      if (is_temporarily_closed == null) {
+        status = "Good"
+      } else if (is_temporarily_closed == 0) {
+        status = "Medium"
+      }
+      newFeaturesList.push({
+        type: "Feature",
+        geometry: {
+          type: "Point",
+          coordinates: [Number(lng), Number(lat)]
+        },
+        properties: {
+          id,
+          name: name,
+          description: `description for Waffle House #${id}`,
+          status,
+        }
+      });
+    }
+    return {
+      type: "FeatureCollection",
+      features: newFeaturesList
+    };
+  };
+  // let results = await fetch(
+  //   'https://wafflehouse.locally.com/stores/conversion_data?has_data=true&company_id=117995&map_distance_diag=99999999',
+  // ).then((data) => data.json());
+  // let waffleHouses = results.markers;
+  // let data = formatData(waffleHouses);
+  // fs.writeFile('wafflehouse.json', JSON.stringify(data), 'utf8', () => null);
+
   return {
     props: {
-      waffleHouses,
       token: process.env.MapboxAccessToken,
     },
   };
