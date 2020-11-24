a82c0cc1b03cb222dd2fe850dc6958a3004082f9
Michael Eliot <michaeleliot@Michaels-MacBook-Pro.local>
2020-11-23 23:15:28 -0800

    hotfix: added protocol to url

--- constants/url.ts
+++ constants/url.ts
@@ -1,1 +1,1 @@
-export default process.env.NODE_ENV == 'development' ? 'http://localhost:3000' : 'wafflehouseindex.vercel.app'
+export default process.env.NODE_ENV == 'development' ? 'http://localhost:3000' : 'https://wafflehouseindex-66urybyng.vercel.app'
